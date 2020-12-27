c2de73d2c65195593859f7869409bf98370c262e
odremaer <mr.jeka0104@gmail.com>
2020-12-27 22:28:34 +1000

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: "beginner")
+Test.create(title: "Ruby", category_id: 1)
+Question.create(body: "vopros po ruby 1", test_id: 1)
+Question.create(body: "vopros po ruby 2", test_id: 1)
+Answer.create(answer: "otvet na 1 vopros po ruby")
+Answer.create(answer: "otvet na 2 vopros po ruby")
+
+Category.create(title: "admin")
+User.create(name: "administrator", password: 'qwe', category_id: 2)
+
+Category.create(title: "user")
+User.create(name: "john", password: 'qwe', category_id: 3)
