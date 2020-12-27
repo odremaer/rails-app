b3c4ae8b060fbd05ab2b7df3d485e9c8a09eb1b0
odremaer <mr.jeka0104@gmail.com>
2020-12-27 23:19:23 +1000

    removed category relation

--- db/migrate/20201226153715_create_users.rb
+++ db/migrate/20201226153715_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name, null: false
       t.string :password, null: false
-      t.references :category, foreign_key: true, null: false
       t.timestamps
     end
   end
