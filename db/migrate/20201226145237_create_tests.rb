0b59108a1812fa9b1e28d22e7a0b5aaaadc6017d
odremaer <mr.jeka0104@gmail.com>
2020-12-27 23:20:36 +1000

    relation between test and user

--- db/migrate/20201226145237_create_tests.rb
+++ db/migrate/20201226145237_create_tests.rb
@@ -3,7 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: 0
-      t.references :category, foreign_key:true, null:false
+      t.references :category, foreign_key: true, null:false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
