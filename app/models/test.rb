ac4c9cba7e1afadadc0bfc2a0c25890a87d8ac2d
odremaer <mr.jeka0104@gmail.com>
2021-01-14 14:51:09 +1000

    method that returns tests with certain category name created

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.order_by_category(title)
+    category_id = Category.where("title = :title", title: title)[0].id
+    Test.where("category_id = :category_id", category_id: category_id).order(id: :desc)
+  end
 end
