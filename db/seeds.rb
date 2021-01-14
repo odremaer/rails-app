8bbdf513d51c0791a0588e3c8ad069c2bfd5469b
odremaer <59476419+odremaer@users.noreply.github.com>
2021-01-14 06:03:00 +0300

    Five models created (#2)
    * created 5 models, information about them in pull request description
    
    * seeds created
    
    * add one-to-many relation between category and tests, same relation between category and users, and again same relation between test and questions
    
    * changes in seeds(add atribute question_id to answer model)
    
    * removed category relation
    
    * relation between test and user
    
    * relation between answer and question
    
    * schema update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: "beginner")
+User.create(name: "john", password: "qwe")
+Test.create(title: "Ruby", category_id: 1, user_id: 1)
+Question.create(body: "vopros po ruby 1", test_id: 1)
+Question.create(body: "vopros po ruby 2", test_id: 1)
+Answer.create(answer: "otvet na 1 vopros po ruby", question_id: 1)
+Answer.create(answer: "otvet na 2 vopros po ruby", question_id: 2)
