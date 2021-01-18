4cc42e81b0731dab4b62445b3fb83288ebe2e842
odremaer <mr.jeka0104@gmail.com>
2021-01-18 17:58:32 +1000

    method user_tests that returns a list of user tests that he passed on certain level created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   def user_tests(level)
-    Test.where("level = :level AND user_id = :id", level: level, id: id)
+    tests_id = []
+    formatted_tests = []
+    Result.joins(:user).where("results.user_id = :id", id: id).each { |cur_result| tests_id << cur_result.test_id }
+    formatted_tests << tests_id.each { |elem| elem.to_s}
+    res = []
+    Test.where(id: formatted_tests, level: level).each { |cur_test| res << cur_test.title }
+    res
   end
 end
