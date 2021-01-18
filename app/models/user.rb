7f066f2cc2f696795b830c435643556bb892d6e4
odremaer <mr.jeka0104@gmail.com>
2021-01-19 03:21:50 +1000

    user_tests renamed to passed_tests, now works without models associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ApplicationRecord
-  def user_tests(level)
-    tests_id = []
-    formatted_tests = []
-    Result.joins(:user).where("results.user_id = :id", id: id).each { |cur_result| tests_id << cur_result.test_id }
-    formatted_tests << tests_id.each { |elem| elem.to_s}
-    res = []
-    Test.where(id: formatted_tests, level: level).each { |cur_test| res << cur_test.title }
-    res
+  def passed_tests(level)
+    Test.where("level = :level", level: level).\
+    joins("INNER JOIN 'results' ON 'results'.'test_id' = 'tests'.'id'").\
+    where("results.user_id = :id", id: id).pluck(:title)
   end
 end
