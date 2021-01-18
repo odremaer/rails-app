7dee529804e289441a5473acec0473071f87666c
odremaer <mr.jeka0104@gmail.com>
2021-01-18 17:57:07 +1000

    removed unnecessary code

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   def self.order_by_category(title)
     result = []
-    Test.joins(:category).where("categories.title = :title", title: title).order(id: :desc).select(:title).each { |cur_test| result << cur_test.title }
+    Test.joins(:category).where("categories.title = :title", title: title).order(id: :desc).each { |cur_test| result << cur_test.title }
     result
   end
 end
