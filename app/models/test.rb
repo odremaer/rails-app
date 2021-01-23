e6e115f4841f90d91ddef8393367601c21e196ae
odremaer <mr.jeka0104@gmail.com>
2021-01-24 03:08:55 +1000

    association created: between author(user) and tests, category and tests, questions and test, tests and users that passed it(through result model)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  belongs_to :user
+  has_many :results
+  has_many :users, through: :results
   def self.certain_category(title)
     Test
     .joins("JOIN categories on categories.id = tests.category_id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :written_tests, class_name: "Test"
+  has_many :results
+  has_many :tests, through: :results
   def passed_tests(level)
     Test
     .joins("JOIN results ON tests.id = results.test_id")
