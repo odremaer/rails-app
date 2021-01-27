7d6b9cfe1f0504d521129cbf6cebb6e01acee1af
odremaer <mr.jeka0104@gmail.com>
2021-01-28 01:52:12 +1000

    certain_category method moved to scope-methods, new scope method selection_by_level, validates if: level is positive number, test with given title and level is unique, title nil or blank

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,28 @@
 class Test < ApplicationRecord
+  scope :certain_category, -> (title){
+                                joins(:category)
+                               .where(categories: { title: title })
+                               .order(id: :desc).pluck(:title)
+                           }
+
   belongs_to :category
   belongs_to :user
+
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
-  def self.certain_category(title)
-    Test
-    .joins(:category)
-    .where(categories: { title: title })
-    .order(id: :desc)
-    .pluck(:title)
+
+  validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
+  validates :title, presence: true
+  def self.selection_by_level(level)
+    case level
+    when 'easy'
+      where(level: 0..1)
+    when 'medium'
+      where(level: 2..4)
+    when 'hard'
+      where(level: 5..Float::INFINITY)
+    end
   end
 end
