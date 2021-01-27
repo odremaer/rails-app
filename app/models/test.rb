498ddb2c73f0968567c48444db348471bf1e3092
odremaer <mr.jeka0104@gmail.com>
2021-01-28 03:02:31 +1000

    created scopes for certain category of levels, scope 'certain_category' realized as class method because it returns an array

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,7 @@
 class Test < ApplicationRecord
-  scope :certain_category, -> (title){
-                                joins(:category)
-                               .where(categories: { title: title })
-                               .order(id: :desc).pluck(:title)
-                           }
+  scope :easy_levels, -> {where(level: 0..1)}
+  scope :medium_levels, -> {where(level: 2..4)}
+  scope :hard_levels, -> {where(level: 5..Float::INFINITY)}
 
   belongs_to :category
   belongs_to :user
@@ -15,14 +13,12 @@ class Test < ApplicationRecord
   validates :level, numericality: { greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level }
   validates :title, presence: true
-  def self.selection_by_level(level)
-    case level
-    when 'easy'
-      where(level: 0..1)
-    when 'medium'
-      where(level: 2..4)
-    when 'hard'
-      where(level: 5..Float::INFINITY)
-    end
+
+  def self.certain_category(title)
+    Test
+    .joins(:category)
+    .where(categories: { title: title })
+    .order(id: :desc)
+    .pluck(:title)
   end
 end
