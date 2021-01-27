67d383cd1d0480d2d7e22b1b9a8a821e20959dbc
odremaer <mr.jeka0104@gmail.com>
2021-01-28 01:52:37 +1000

    validates if name and password blank or nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-  has_many :written_tests, class_name: "Test", dependent: :destroy
+  has_many :written_tests, class_name: 'Test', dependent: :destroy
   has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent:  :destroy
+  has_many :tests, through: :results, dependent: :destroy
+
+  validates :name, :password, presence: true
+  
   def passed_tests(level)
     Test
-    .joins(:results)
-    .where(level: level, results: { user_id: id })
+      .joins(:results)
+      .where(level: level, results: { user_id: id })
   end
 end
