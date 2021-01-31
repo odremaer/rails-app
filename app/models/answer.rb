494ea35774f8e05b0ed33e40f9ee1ca10500ac8e
odremaer <59476419+odremaer@users.noreply.github.com>
2021-01-31 15:32:22 +0300

    Validations and scope methods (#6)
    * scope method for correct answers, validation for: answer isnt nil or blank, correct amount of answers for one question
    
    * default scope for order(ascending by title), validates if title nil or blank
    
    * validates if body nil or blank
    
    * certain_category method moved to scope-methods, new scope method selection_by_level, validates if: level is positive number, test with given title and level is unique, title nil or blank
    
    * validates if name and password blank or nil
    
    * fixed mispel => to >=
    
    * created scopes for certain category of levels, scope 'certain_category' realized as class method because it returns an array

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
+  scope :correct, -> { where(correct: true) }
+
   belongs_to :question
+
+  validate :validate_answers_amount
+  validates :answer, presence: true
+
+  private
+
+  def validate_answers_amount
+    if question.answers.count >= 4
+      errors.add(:answer, "amount of answers should be between 1..4")
+    end
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,19 @@
 class Test < ApplicationRecord
+  scope :easy_levels, -> {where(level: 0..1)}
+  scope :medium_levels, -> {where(level: 2..4)}
+  scope :hard_levels, -> {where(level: 5..Float::INFINITY)}
+
   belongs_to :category
   belongs_to :user
+
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
+
+  validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
+  validates :title, presence: true
+
   def self.certain_category(title)
     Test
     .joins(:category)
