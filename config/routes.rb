85f26d22d032607181d882030fbd435d3e45bc75
odremaer <mr.jeka0104@gmail.com>
2021-02-01 15:07:20 +1000

    created resources: tests and questions, appoint my own path to creating new question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tests do
+    resources :questions, only: [:index, :show, :create, :destroy]
+  end
+  get 'questions/new', to: 'questions#new'
 end
