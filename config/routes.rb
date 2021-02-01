108560c57bc1b1f11112cd9e38d8ef96aaf32a45
odremaer <mr.jeka0104@gmail.com>
2021-02-01 18:27:58 +1000

    shallow: true for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, only: [:index, :show, :create, :destroy]
+    resources :questions, shallow: true
   end
-  get 'questions/new', to: 'questions#new'
 end
