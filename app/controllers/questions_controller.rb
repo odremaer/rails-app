3522054629df73bc19bbfd620ab14d44306d3187
odremaer <mr.jeka0104@gmail.com>
2021-02-02 18:58:34 +1000

    appoint that callback 'find_test' should work only for #index, created new callback 'find_question', #create redirecting to list of test_questions if question is saved,

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
-  skip_before_action :find_test, only: [:new, :show, :destroy]
+  before_action :find_test, only: [:index]
+  before_action :find_question, only: [:show, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
@@ -8,23 +8,21 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @question = Question.create!(body: question_params, test_id: params[:test_id])
+    @question = Question.new(body: question_params, test_id: params[:test_id])
     if @question.save
-      render plain: @question.inspect
+      redirect_to test_questions_path
     else
       render plain: "Вопрос не удалось сохранить"
     end
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
     render plain: "Вопрос успешно удален"
   end
@@ -33,6 +31,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
