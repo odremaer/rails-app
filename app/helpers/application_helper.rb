058a543bd771555e3d95cf1937235b48b33748c3
odremaer <mr.jeka0104@gmail.com>
2021-02-04 03:05:07 +1000

    #current_year shows in which city returned year is, #github_url works with helper #link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
  def current_year
-   Time.now.year
+   "#{Time.zone.now.year}г. в городе #{Time.zone.name}"
  end
 
  def github_url(author, repo)
-   res = "https://github.com/#{author}/#{repo}"
+   link_to "Проект", "https://github.com/#{author}/#{repo}", rel: "nofollow", target: "_blank"
  end
 
 end
