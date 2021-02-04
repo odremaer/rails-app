8b5a981c3d1da8fa6e72a950df7e21e231b278fb
odremaer <mr.jeka0104@gmail.com>
2021-02-04 17:02:42 +1000

    removed unnecessary callback from test_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,18 +1,16 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show]
+  
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
-  def show; end
+  def show
+    @test = Test.find(params[:id])
+  end
 
   def index
     @tests = Test.all
   end
 
   private
-
-  def find_test
-    @test = Test.find(params[:id])
-  end
 
   def rescue_with_test_not_found
     render plain: 'Запрашиваемого теста не найдено'
