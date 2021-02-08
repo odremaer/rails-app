5fd2aa3f02a728a92f92899ee6ae891d16a1ceb5
odremaer <mr.jeka0104@gmail.com>
2021-02-08 21:03:01 +1000

    model test_passages created, other models changed according to test_passages name(information about test_passages model content in pull request description)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   has_many :written_tests, class_name: 'Test', dependent: :destroy
-  has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
 
   validates :name, :password, presence: true
-  
+
   def passed_tests(level)
     Test
-      .joins(:results)
-      .where(level: level, results: { user_id: id })
+      .joins(:test_passages)
+      .where(level: level, test_passages: { user_id: id })
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
