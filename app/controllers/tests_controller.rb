eaf023b68d398096c0b75f728de62478979008df
odremaer <mr.jeka0104@gmail.com>
2021-02-08 21:04:28 +1000

    #start created in TestsController

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -10,6 +10,13 @@ class TestsController < ApplicationController
     @tests = Test.all
   end
 
+  def start
+    @test = Test.find(params[:id])
+    @user = User.first
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
   def rescue_with_test_not_found
