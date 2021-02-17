90ca9931ad07b8d2c4bcfb3bf4933d31c51c91b5
odremaer <mr.jeka0104@gmail.com>
2021-02-17 18:50:23 +1000

    name -> email, password -> password_digest in create_users migration(and schema update)

--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 2021_02_08_060816) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "password", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
