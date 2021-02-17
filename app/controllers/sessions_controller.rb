c96c64f9debc19d24598ff9927f206df086b39e2
odremaer <mr.jeka0104@gmail.com>
2021-02-17 18:59:54 +1000

    session controller created, #create redirecting user to previous link when he signed in, #destroy serve to log out, and views for controller created

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to session[:previous_link]
+    else
+      flash.now[:alert] = 'Are you a Guru? Verify your email and password'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to login_path
+  end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+ def flash_message 
+   if flash[:alert]
+     content_tag :p, flash[:alert], class: 'flash alert'
+   end
+ end
 
  def current_year
    "Current year - #{Time.current.year}"
