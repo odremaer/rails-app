5b65c1a19a88d467280546f96d6c6e7ff00c085f
odremaer <mr.jeka0104@gmail.com>
2021-03-06 13:43:53 +0300

    removed #all_categories_title from category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,6 @@ class Category < ApplicationRecord
   default_scope { order(title: :asc) }
 
   has_many :tests, dependent: :destroy
-  
-  validates :title, presence: true
 
-  def self.all_categories_title
-    Category.all.pluck(:title)
-  end
+  validates :title, presence: true
 end
