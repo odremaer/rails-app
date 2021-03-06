eea816ed3243833d1e3925c5a915dcdf7e12b29b
odremaer <mr.jeka0104@gmail.com>
2021-03-07 01:26:00 +0300

    removed validation code from badge model, moved it to Validations module, moved methods from user model to places where they needed(rules services)

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,51 +2,8 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges, dependent: :destroy
 
-  validate :validate_first_attempt_absence_if_category_or_level_picked
-  validate :validate_if_attrs_are_empty
-  validate :validate_if_rule_param_is_number_when_rule_type_is_level
-  validate :validate_category_presence
-  validates :title, presence: true
+  include Validations
 
   IMAGES = Dir.children(Rails.root.join('app', 'assets', 'images', 'badges').to_s)
   RULE_TYPES = ['category', 'level', 'first attempt'].freeze
-
-  def contains_level_rule?
-    rule_type == 'level'
-  end
-
-  def contains_category_rule?
-    rule_type == 'category'
-  end
-
-  def contains_first_attempt_rule?
-    rule_type == 'first attempt'
-  end
-
-  private
-
-  # if user dont have js enabled
-  def validate_first_attempt_absence_if_category_or_level_picked
-    if contains_first_attempt_rule? && rule_parameter.present?
-      errors.add(:badge, I18n.t('badges.errors.first_attempt_with_other_attrs'))
-    end
-  end
-
-  def validate_if_attrs_are_empty
-    unless rule_type == 'first attempt'
-      errors.add(:badge, I18n.t('badges.errors.empty_attr')) if rule_parameter.blank?
-    end
-  end
-
-  def validate_if_rule_param_is_number_when_rule_type_is_level
-    if rule_type == 'level' && /[a-zA-Z]/ === rule_parameter
-      errors.add(:badge, I18n.t('badges.errors.rule_param_should_be_number'))
-    end
-  end
-
-  def validate_category_presence
-    if rule_type == 'category'
-      errors.add(:badge, I18n.t('badges.errors.category_presence')) unless Category.pluck(:title).include?(rule_parameter)
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,24 +24,6 @@ class User < ApplicationRecord
       .where(level: level, test_passages: { user_id: id })
   end
 
-  def successfully_passed_tests_by_level(level, test)
-    Test
-      .joins(:test_passages)
-      .where(level: level, test_passages: { user_id: id, correct_questions: test.questions.count })
-  end
-
-  def successfully_passed_tests_by_category(category, test)
-    Test
-      .joins(:test_passages)
-      .where(category_id: category.id, test_passages: { user_id: id, correct_questions: test.questions.count })
-  end
-
-  def successfully_passed_tests(test)
-    Test
-      .joins(:test_passages)
-      .where(test_passages: { user_id: id, correct_questions: test.questions.count })
-  end
-
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
