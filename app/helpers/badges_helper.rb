f430c1bdd963c078f0bf489b9a8c07d778541274
odremaer <mr.jeka0104@gmail.com>
2021-03-06 17:06:39 +0300

    badges helper setted up according to new badge model columns

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,11 +1,9 @@
 module BadgesHelper
   def create_rule(badge)
-    if badge.contains_category_and_level_rule?
-      content_tag :td, I18n.t('badges.rules.category_and_level_rule', level: badge.level, category: badge.category)
-    elsif badge.contains_category_rule?
-      content_tag :td, I18n.t('badges.rules.category_rule', category: badge.category)
+    if badge.contains_category_rule?
+      content_tag :td, I18n.t('badges.rules.category_rule', category: badge.rule_parameter)
     elsif badge.contains_level_rule?
-      content_tag :td, I18n.t('badges.rules.level_rule', level: badge.level)
+      content_tag :td, I18n.t('badges.rules.level_rule', level: badge.rule_parameter)
     elsif badge.contains_first_attempt_rule?
       content_tag :td, I18n.t('badges.rules.first_attempt_rule')
     end
